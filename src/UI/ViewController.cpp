4c29f7fd8cf97857a8e0c7ef83ff02bcddb5201a
CGray1234 <102427011+CGray1234@users.noreply.github.com>
2022-10-15 13:10:56 -0400

    Update ViewController.cpp

--- src/UI/ViewController.cpp
+++ src/UI/ViewController.cpp
@@ -3,6 +3,9 @@
 #include "questui/shared/BeatSaberUI.hpp"
 #include "questui/shared/QuestUI.hpp"
 
+#include "UnityEngine/Resources.hpp"
+#include "GlobalNamespace/MenuTransitionsHelper.hpp"
+
 DEFINE_TYPE(Sqores, SqoreViewController);
 
 using namespace QuestUI::BeatSaberUI;
@@ -14,10 +17,24 @@ void SqoreViewController::DidActivate(bool firstActivation, bool addedToHierarch
 
         UnityEngine::GameObject *container = CreateScrollView(get_transform());
 
-        auto scoreToggle = CreateToggle(container->get_transform(), "Enable/disable scores", getModConfig().ScoresEnabled.GetValue(), 
+        auto scoreToggle = CreateToggle(container->get_transform(), "Enable/disable scores (don't use this toggle, use the other one please. I'll be removing this soon)", getModConfig().ScoresEnabled.GetValue(), 
             [=](bool value) {
                 getModConfig().ScoresEnabled.SetValue(value);
+
+                getModConfig().RestartRequired.SetValue(true);
             }
         );
+    }
+}
+
+using namespace UnityEngine;
+using namespace GlobalNamespace;
+
+void SqoreViewController::DidDeactivate(bool removedFromHierarchy, bool screenSystemDisabling) {
+
+    if (getModConfig().RestartRequired.GetValue() == true) {
+        Resources::FindObjectsOfTypeAll<MenuTransitionsHelper*>().FirstOrDefault()->RestartGame(nullptr);
+
+        getModConfig().RestartRequired.SetValue(false);
     }
 }
