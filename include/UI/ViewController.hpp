26311b2c00ca741ef9e65f82a7be697e12a916d6
CGray1234 <102427011+CGray1234@users.noreply.github.com>
2022-10-15 13:10:19 -0400

    Update ViewController.hpp

--- include/UI/ViewController.hpp
+++ include/UI/ViewController.hpp
@@ -6,4 +6,5 @@
 DECLARE_CLASS_CODEGEN(Sqores, SqoreViewController, HMUI::ViewController,
 
     DECLARE_OVERRIDE_METHOD(void, DidActivate, il2cpp_utils::FindMethodUnsafe("HMUI", "ViewController", "DidActivate", 3), bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
+    DECLARE_OVERRIDE_METHOD(void, DidDeactivate, il2cpp_utils::FindMethodUnsafe("HMUI", "ViewController", "DidDeactivate", 2), bool removedFromHierarchy, bool screenSystemDisabling);
 )
