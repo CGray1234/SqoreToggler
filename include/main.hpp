1d942603a33478da89e0f7e643eae56f9139cc41
CGray1234 <102427011+CGray1234@users.noreply.github.com>
2022-10-15 13:14:18 -0400

    Update main.hpp

--- include/main.hpp
+++ include/main.hpp
@@ -13,3 +13,4 @@
 // Define these functions here so that we can easily read configuration and log information from other files
 Configuration& getConfig();
 Logger& getLogger();
+extern ModInfo modInfo;
